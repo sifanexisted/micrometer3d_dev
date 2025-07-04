69fb5a73bed7964cb6ca846b334b69c87c4252cb
Sifan Wang <sifanw@sas.upenn.edu>
2025-07-04 17:25:57 -0400

    3d

---
+++ models/cvit3d.py
@@ -0,0 +1,123 @@
+import jax
+import jax.numpy as jnp
+
+import flax.linen as nn
+
+from einops import rearrange, repeat
+
+from .vit3d import Encoder, CrossAttnBlock
+from .fno3d import FourierStage
+
+from typing import Optional, Callable, Dict
+
+
+class FNOEncoder(nn.Module):
+    emb_dim: int = 64
+    modes1: int = 32
+    modes2: int = 32
+    modes3: int = 32
+    depth: int = 4
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(self.emb_dim)(x)
+
+        for _ in range(self.depth):
+            x = FourierStage(
+                emb_dim=self.emb_dim,
+                modes1=self.modes1,
+                modes2=self.modes2,
+            )(x)
+
+        return x
+
+
+def compute_dist_weights(coords, grid, eps):
+    """
+    coords: (b, n, 3)
+    grid: (h * w * c, 3)
+    eps: float
+    output: (n, h * w * c)
+    """
+    # Compute distance between coords and grid
+    d2 = ((coords[:, jnp.newaxis, :] - grid[jnp.newaxis, :, :]) ** 2).sum(axis=2)
+    # Compute weights using exponential kernel
+    w = jnp.exp(-eps * d2) / jnp.exp(-eps * d2).sum(axis=1, keepdims=True)
+    return w
+
+
+class CViT(nn.Module):
+    patch_size: tuple = (16, 16, 4)
+    grid_size: tuple = (128, 128, 32)
+    fourier_emb_dim: int = 128
+    fourier_modes: int = 32
+    fourier_depth: int = 3
+    emb_dim: int = 256
+    num_heads: int = 16
+    depth: int = 12
+    dec_emb_dim: int = 256
+    dec_num_heads: int = 8
+    dec_depth: int = 1
+    mlp_ratio: int = 1
+    out_dim: int = 1
+    eps: float = 1e5
+    layer_norm_eps: float = 1e-5
+    model_name: Optional[str] = None
+
+    def setup(self):
+        # Create grid and latents
+        n_x, n_y, n_z = self.grid_size[0], self.grid_size[1], self.grid_size[2]
+        x = jnp.linspace(0, 1, n_x)
+        y = jnp.linspace(0, 1, n_y)
+        z = jnp.linspace(0, 1, n_z)
+        xx, yy, zz = jnp.meshgrid(x, y, z, indexing="ij")
+        self.grid = jnp.hstack([xx.flatten()[:, None], yy.flatten()[:, None], zz.flatten()[:, None]])
+
+    @nn.compact
+    def __call__(self, x, coords):
+        b, h, w, c = x.shape
+
+        # Lift inputs to latent space by FNO layers
+        x = FNOEncoder(
+            depth=self.fourier_depth,
+            emb_dim=self.fourier_emb_dim,
+            modes1=self.fourier_modes,
+            modes2=self.fourier_modes,
+            modes3=self.fourier_modes,
+        )(x)
+
+        # If input size is different from grid size, resize input to grid size
+        if (h, w, c) != self.grid_size:
+            x = jax.image.resize(x, (b, *self.grid_size, c), method="bilinear")
+
+        w = compute_dist_weights(coords, self.grid, self.eps)  # (n, h * w)
+        w = rearrange(w, "n (h w d) -> n h w d", h=self.grid_size[0], w=self.grid_size[1], d=self.grid_size[2])
+
+        # Interpolate latents using weights
+        coords = jnp.einsum("bhwdc,nhwd->bnc", x, w)
+        coords = nn.Dense(self.dec_emb_dim)(coords)
+        coords = nn.LayerNorm(epsilon=self.layer_norm_eps)(coords)
+
+        x = Encoder(
+            self.patch_size,
+            self.emb_dim,
+            self.depth,
+            self.num_heads,
+            self.mlp_ratio,
+            self.layer_norm_eps,
+        )(x)
+        x = nn.Dense(self.dec_emb_dim)(x)
+
+        # Decoder
+        for _ in range(self.dec_depth):
+            x = CrossAttnBlock(
+                num_heads=self.dec_num_heads,
+                emb_dim=self.dec_emb_dim,
+                mlp_ratio=self.mlp_ratio,
+                layer_norm_eps=self.layer_norm_eps,
+            )(coords, x)
+
+        x = nn.LayerNorm(epsilon=self.layer_norm_eps)(x)
+        x = nn.Dense(self.out_dim)(x)
+
+        return x
---
+++ models/fno3d.py
@@ -0,0 +1,251 @@
+import jax.numpy as jnp
+from jax import random
+
+import flax.linen as nn
+from typing import Optional, Callable, Tuple
+from .vit3d import MlpBlock
+
+
+def normal(stddev=1e-2, dtype=jnp.float32) -> Callable:
+    def init(key, shape, dtype=dtype):
+        keys = random.split(key)
+        return random.normal(keys[0], shape) * stddev
+
+    return init
+
+
+def complex_mul3d(x, weights_r, weights_i):
+    """
+    x: (b, h, w, d, c)
+    weights_r: (c, out_dim, modes1, modes2, modes3)
+    weights_i: (c, out_dim, modes1, modes2, modes3)
+    out: (b, h, w, d, out_dim)
+    """
+    return jnp.einsum("bxyzc,coxyz->bxyzo", x, weights_r + 1j * weights_i)
+
+
+# let's use this module to store weights
+# essentially, we'll also use this when working with tucker tensorized weights
+class FNO3DWeights(nn.Module):
+    in_dim: int = 32
+    out_dim: int = 32
+    modes1: int = 12
+    modes2: int = 12
+    modes3: int = 12
+    factorized: bool = False
+    ranks: Optional[Tuple[float]] = None
+    model_name: Optional[str] = None
+
+    def setup(self):
+        scale = 1 / (self.in_dim * self.out_dim)
+
+        self.weights1_r = self.param(
+            "weights1_r",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights1_i = self.param(
+            "weights1_i",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights2_r = self.param(
+            "weights2_r",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights2_i = self.param(
+            "weights2_i",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+
+        self.weights3_r = self.param(
+            "weights3_r",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights3_i = self.param(
+            "weights3_i",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights4_r = self.param(
+            "weights4_r",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+        self.weights4_i = self.param(
+            "weights4_i",
+            normal(scale, jnp.float32),
+            (self.in_dim, self.out_dim, self.modes1, self.modes2, self.modes3),
+            jnp.float32,
+        )
+
+    def __call__(self):
+        return self.weights1_r, self.weights1_i, self.weights2_r, self.weights2_i, self.weights3_r, self.weights3_i, self.weights4_r, self.weights4_i
+
+
+class SpectralConv3d(nn.Module):
+    out_dim: int = 32
+    modes1: int = 12
+    modes2: int = 12
+    modes3: int = 12
+
+    @nn.compact
+    def __call__(self, x):
+        # x.shape: (b, h, w, d, c)
+
+        # Initialize parameters
+        in_dim = x.shape[-1]
+        h = x.shape[1]
+        w = x.shape[2]
+        d = x.shape[3]
+
+        # Checking that the modes are not more than the input size
+        assert self.modes1 <= h // 2 + 1
+        assert self.modes2 <= w // 2 + 1
+        assert self.modes3 <= d // 2 + 1
+
+        weights1_r, weights1_i, weights2_r, weights2_i, weights3_r, weights3_i, weights4_r, weights4_i = FNO3DWeights(
+            in_dim=in_dim,
+            out_dim=self.out_dim,
+            modes1=self.modes1,
+            modes2=self.modes2,
+            modes3=self.modes3,
+            factorized=False,  # TODO: create as input parameter
+            ranks=(1, 1, 0.25, 0.25, 0.25),  # TODO: create as input parameter
+            model_name='FNO3DWeights'
+        )()
+
+        # Compute Fourier coefficients
+        x_ft = jnp.fft.rfftn(x, axes=(1, 2, 3))
+        # Multiply relevant Fourier modes
+        out_ft = jnp.zeros_like(x_ft)
+
+        out_ft = out_ft.at[:, : self.modes1, : self.modes2, : self.modes3, :].set(
+            complex_mul3d(
+                x_ft[:, : self.modes1, : self.modes2, : self.modes3, :],
+                weights1_r,
+                weights1_i,
+            )
+        )
+        out_ft = out_ft.at[:, -self.modes1:, : self.modes2, : self.modes3, :].set(
+            complex_mul3d(
+                x_ft[:, -self.modes1:, : self.modes2, : self.modes3, :],
+                weights2_r,
+                weights2_i,
+            )
+        )
+        out_ft = out_ft.at[:, : self.modes1, -self.modes2:, : self.modes3, :].set(
+            complex_mul3d(
+                x_ft[:, : self.modes1, -self.modes2:, : self.modes3, :],
+                weights3_r,
+                weights3_i,
+            )
+        )
+        out_ft = out_ft.at[:, -self.modes1:, -self.modes2:, : self.modes3, :].set(
+            complex_mul3d(
+                x_ft[:, -self.modes1:, -self.modes2:, : self.modes3, :],
+                weights4_r,
+                weights4_i,
+            )
+        )
+
+        # Return to physical space
+        x = jnp.fft.irfftn(out_ft, axes=(1, 2, 3), s=(h, w, d))
+        return x
+
+
+# trying out some changes from https://arxiv.org/pdf/2310.00120
+class FourierStage(nn.Module):
+    emb_dim: int = 32
+    modes1: int = 12
+    modes2: int = 12
+    modes3: int = 12
+    activation: Callable = nn.gelu
+    factorized: bool = False
+
+    @nn.compact
+    def __call__(self, x):
+        x_fourier = SpectralConv3d(
+            out_dim=self.emb_dim,
+            modes1=self.modes1,
+            modes2=self.modes2,
+            modes3=self.modes3,
+        )(x)
+
+        x_fourier = nn.LayerNorm()(x_fourier)
+        x_local = nn.Conv(
+            self.emb_dim,
+            (1, 1, 1),
+        )(x)
+
+        x1 = self.activation(x_fourier + x_local)
+        x2 = MlpBlock(self.emb_dim, self.emb_dim)(x1)
+        x2 = nn.LayerNorm()(x2)
+
+        x2_local = nn.Conv(
+            self.emb_dim,
+            (1, 1, 1),
+        )(x)
+        x3 = self.activation(x2 + x2_local)
+        return x3
+
+
+class FNO3d(nn.Module):
+    modes1: int = 12
+    modes2: int = 12
+    modes3: int = 12
+    emb_dim: int = 32
+    out_dim: int = 1
+    depth: int = 4
+    activation: Callable = nn.gelu
+    padding: int = 0  # Padding for non-periodic inputs
+    model_name: Optional[str] = None
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        # Lift the input to a higher dimension
+        x = nn.Dense(self.emb_dim)(x)
+
+        # Pad input
+        if self.padding > 0:
+            x = jnp.pad(
+                x,
+                (
+                    (0, 0),
+                    (0, self.padding),
+                    (0, self.padding),
+                    (0, self.padding),
+                    (0, 0),
+                ),
+                mode="constant",
+            )
+
+        for _ in range(self.depth):
+            x = FourierStage(
+                emb_dim=self.emb_dim,
+                modes1=self.modes1,
+                modes2=self.modes2,
+                modes3=self.modes3,
+                activation=self.activation,
+            )(x)
+
+        # Unpad
+        if self.padding > 0:
+            x = x[:, : -self.padding, : -self.padding, : -self.padding, :]
+
+        # Project to the output dimension
+        x = nn.Dense(self.emb_dim)(x)
+        x = self.activation(x)
+        x = nn.Dense(self.out_dim)(x)
+
+        return x
